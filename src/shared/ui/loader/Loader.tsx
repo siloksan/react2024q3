ca000215341d1045b7ba17e62c88ad153a4d7caa
siloksan <tutel07@gmail.com>
2024-07-04 20:09:51 +0300

    refactor: convert Loader into functional component

--- src/shared/ui/loader/Loader.tsx
+++ src/shared/ui/loader/Loader.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-
 import styles from './Loader.module.scss';
 
-class Loader extends React.PureComponent {
-  render() {
-    return (
-      <div className={styles.container}>
-        <span className={styles.loader} />
-      </div>
-    );
-  }
+function Loader() {
+  return (
+    <div className={styles.container}>
+      <span className={styles.loader} />
+    </div>
+  );
 }
 
 export default Loader;
