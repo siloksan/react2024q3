16536fa81f3e4208c39659288592fa33583e140c
siloksan <tutel07@gmail.com>
2024-07-04 15:55:01 +0300

    refactor: convert Error Button into functional component

--- src/widgets/header/components/errorButton/ErrorButton.tsx
+++ src/widgets/header/components/errorButton/ErrorButton.tsx
@@ -1,34 +1,20 @@
-import React from 'react';
-
+import { useState } from 'react';
 import styles from './ErrorButton.module.scss';
 
-interface Props {}
-interface State {
-  hasError: boolean;
-}
-class ErrorButton extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      hasError: false,
-    };
+function ErrorButton() {
+  const [error, setError] = useState(false);
+  function throwError() {
+    setError(true);
   }
 
-  throwError = () => {
-    this.setState({ hasError: true });
-  };
-
-  render() {
-    const { hasError } = this.state;
-    if (hasError) {
-      throw new Error("It seems like you've broken something!");
-    }
-    return (
-      <button type="button" onClick={this.throwError} className={styles.btn}>
-        throw error
-      </button>
-    );
+  if (error) {
+    throw new Error("It seems like you've broken something!");
   }
+  return (
+    <button type="button" onClick={throwError} className={styles.btn}>
+      throw error
+    </button>
+  );
 }
 
 export default ErrorButton;
