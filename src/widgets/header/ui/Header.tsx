e0076b28ccce8c15fa03209c8051e8dd89279a65
siloksan <tutel07@gmail.com>
2024-07-13 17:00:53 +0300

    fix: close card details correctly

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import logo from '../assets/startrek-logo.png';
 import ErrorButton from '../components/errorButton/ErrorButton';
@@ -6,9 +6,11 @@ import ErrorButton from '../components/errorButton/ErrorButton';
 import styles from './Header.module.scss';
 
 export default function Header() {
+  const [searchParams] = useSearchParams();
+
   return (
     <header className={styles.container} data-testid="header">
-      <Link to="/">
+      <Link to={`/?${searchParams.toString()}`}>
         <div className={styles.logo}>
           <img src={logo} alt="Star trek logo" />
         </div>
