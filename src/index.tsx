ee77917efed0919f519ad0561b31e0087839de1c
siloksan <tutel07@gmail.com>
2024-07-13 18:32:09 +0300

    chore: setup redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import { store } from 'app/strore';
 import App from 'app/App';
 import ErrorBoundary from 'shared/ui/errorBoundary/ErrorBoundary';
 import 'app/styles/index.scss';
@@ -8,7 +10,9 @@ import 'app/styles/index.scss';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ErrorBoundary>
   </React.StrictMode>
 );
