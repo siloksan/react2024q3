284a464654cb349589b97bf30dd8de9d0df3432f
siloksan <tutel07@gmail.com>
2024-07-15 19:05:38 +0300

    feat: create setupStore function

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,16 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import selectedItems from 'features/reduxSlices/selectedItems';
 import spacecrafts from 'features/reduxSlices/spacecrafts';
 import { starTrekApi } from 'shared/api/services';
 
-export const store = configureStore({
-  reducer: {
-    selectedItems,
-    spacecrafts,
-    [starTrekApi.reducerPath]: starTrekApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(starTrekApi.middleware),
+const rootReducer = combineReducers({
+  selectedItems,
+  spacecrafts,
+  [starTrekApi.reducerPath]: starTrekApi.reducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(starTrekApi.middleware),
+    preloadedState,
+  });
+};
+
+export const store = setupStore();
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
