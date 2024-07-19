2e372dca197e29355b7af10cb400b95caecc814b
siloksan <tutel07@gmail.com>
2024-07-19 10:53:02 +0300

    feat: add dark mode for flyout

--- src/shared/ui/flyout/Flyout.tsx
+++ src/shared/ui/flyout/Flyout.tsx
@@ -6,13 +6,16 @@ import { removeAll } from 'features/reduxSlices/selectedItems';
 import { useEffect, useState } from 'react';
 import spacecrafts from 'features/reduxSlices/spacecrafts';
 import formatData from 'shared/lib/formatData/formatData';
+import { useTheme } from 'app/providers/themeProvider';
 import styles from './Flyout.module.scss';
+import Button from '../button/Button';
 
 export default function Flyout() {
   const selectedItems = useSelector((state: RootState) => state.selectedItems.value);
   const dispatch = useDispatch();
   const numberOfSelectedItems = selectedItems.length;
   const [isUnmount, setIsUnmount] = useState(false);
+  const dark = useTheme();
 
   useEffect(() => {
     if (numberOfSelectedItems > 0) {
@@ -40,18 +43,22 @@ export default function Flyout() {
     containerClassName += ` ${styles.hidden}`;
   }
 
+  if (dark) {
+    containerClassName += ` ${styles.dark}`;
+  }
+
   return (
     <div className={containerClassName} data-testid="flyout">
       <p className={styles.info}>
         The number of selected items - <strong>{numberOfSelectedItems}</strong>
       </p>
       <div className={styles.buttons}>
-        <button type="button" className={styles.button} onClick={unselectAll}>
-          Unselect all
-        </button>
-        <a href={url} download={`${spacecrafts.length}_spacecrafts.csv`}>
-          Download
-        </a>
+        <Button onClick={unselectAll}>Unselect all</Button>
+        <Button>
+          <a href={url} download={`${spacecrafts.length}_spacecrafts.csv`}>
+            Download
+          </a>
+        </Button>
       </div>
     </div>
   );
