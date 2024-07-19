03df9f9ee2f4f88bf2d580493f9d229057ecdc7c
siloksan <tutel07@gmail.com>
2024-07-19 09:47:15 +0300

    feat: add dark mode for search box

--- src/shared/ui/search/SearchBox.tsx
+++ src/shared/ui/search/SearchBox.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import useStorage from 'shared/lib/useStorage/useStorage';
+import { useTheme } from 'app/providers/themeProvider';
 import loupe from './assets/search-icon.svg';
 
 import styles from './SearchBox.module.scss';
+import Button from '../button/Button';
 
 interface Props {
   setSearchTerm: (searchTerm: string) => void;
@@ -13,6 +15,7 @@ interface Props {
 export default function SearchBox({ setSearchTerm, searchTerm, setPageNumber }: Props) {
   const [value, setValue] = useState(searchTerm);
   const { setData } = useStorage();
+  const dark = useTheme();
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
@@ -31,20 +34,29 @@ export default function SearchBox({ setSearchTerm, searchTerm, setPageNumber }: 
     }
   };
 
+  let containerClassName = styles.container;
+  let inputClassName = styles.input;
+  let buttonClassName = styles.button;
+  if (dark) {
+    containerClassName += ` ${styles.dark}`;
+    inputClassName += ` ${styles.dark}`;
+    buttonClassName += ` ${styles.dark}`;
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={containerClassName}>
       <div className={styles.form}>
         <input
           value={value}
           onChange={handleInput}
           onKeyDown={handleKeyDown}
           type="text"
-          className={styles.input}
+          className={inputClassName}
           placeholder="Search"
         />
-        <button className={styles.button} aria-label="Search" type="submit" onClick={() => handleSubmit()}>
+        <Button onClick={handleSubmit} aria-label="Search" type="submit" additionalStyles={buttonClassName}>
           <img src={loupe} alt="loupe icon" />
-        </button>
+        </Button>
       </div>
     </div>
   );
