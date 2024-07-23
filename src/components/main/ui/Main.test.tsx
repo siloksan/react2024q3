d321e97af22e29d76f5c0cf4348511380661efd8
siloksan <tutel07@gmail.com>
2024-07-23 10:19:24 +0300

    feat: add main component

---
+++ src/components/main/ui/Main.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'app/store';
+
+import ErrorBoundary from 'shared/ui/errorBoundary/ErrorBoundary';
+import Main from './Main';
+
+const customRender = () =>
+  render(
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
+
+describe('Main', () => {
+  it('renders Main', () => {
+    customRender();
+
+    const h1 = screen.getByRole('heading', { level: 1 });
+
+    expect(h1).toBeInTheDocument();
+  });
+
+  it.todo('throws an error when the request fails');
+
+  it('renders loader and fetches data successfully', async () => {
+    customRender();
+
+    const loader = screen.getAllByTestId('loader');
+    expect(loader[0]).toBeInTheDocument();
+
+    await waitFor(() => {
+      const cardList = screen.getByTestId('card-list');
+
+      expect(cardList).toBeInTheDocument();
+    });
+  });
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Main from '@/components/main';
 import Head from 'next/head';
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <main>
-        <h1>Hi John!</h1>
+        <Main />
       </main>
     </>
   );
